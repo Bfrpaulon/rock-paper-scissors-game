f25a856bbb860a5540e26ec537795ce175bd7bc7
Bfrpaulon <brunopaulon@outlook.com.br>
2023-03-15 17:48:41 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# rock-paper-scissors-game
+# Rock Paper Scissors
+
+This is a simple Rock, Paper, Scissors game where the user plays against the computer. It was developed using HTML, CSS and JavaScript technologies.
+
+## How to play
+
+When opening the game page, the user must choose between rock, paper or scissors, clicking on the corresponding image. The computer will also make its choice, which will be revealed after an animation.
+
+The result of the game will be shown right after, indicating if the user won, lost or tied.
+
+## How it was done
+
+The game was developed using the following technologies:
+
+- HTML: for structuring the game page
+- CSS: for styling elements and animations
+- JavaScript: for game logic and user interaction
+
+An HTML file was created with the basic structure of the page, including the title, the link to the style file and the game script. Visual content has also been added, such as game option images and the title.
+
+The CSS file was used to style the page and create the animations. The colors, fonts, sizes and placement of the elements were defined. Animations were also created using the CSS transformation and transition properties.
+
+The JavaScript file was used for game logic and user interaction. A function was created to raffle the computer's choice and compare it with the user's choice, defining the result. A function was also created to animate the computer's choice before revealing the result.
+
+## How to run
+
+To play the game, just open the HTML file in a web browser compatible with the technologies used (HTML, CSS and JavaScript). It is recommended to use the latest version of Google Chrome, Mozilla Firefox or Microsoft Edge.
+
+## Conclusion
+
+This game is a simple example of how to use HTML, CSS and JavaScript technologies to create user interaction and develop simple logic. It is possible to add more functionality and complexity to the game, such as scoring, difficulty and different types of moves.
--- assets/script.js
+++ assets/script.js
@@ -2,44 +2,58 @@ const options = document.querySelectorAll('.option');
 const playerOptions = document.querySelector('.player-options');
 const enemyOptions = document.querySelector('.enemy-options');
 const result = document.querySelector('.result');
+let playerScore = 0;
+let enemyScore = 0;
 
 const getRandomOption = () => {
-   const index = Math.floor(Math.random() * options.length);
-   const option = options[index].cloneNode(true);
-   enemyOptions.innerHTML = '';
-   enemyOptions.appendChild(option);
+  const index = Math.floor(Math.random() * options.length);
+  const option = options[index].cloneNode(true);
+  enemyOptions.innerHTML = '';
+  enemyOptions.appendChild(option);
 };
 
 const showResult = (player, enemy) => {
-   const playerOpt = player.getAttribute('opt');
-   const enemyOpt = enemy.getAttribute('opt');
-   let message = '';
+  const playerOpt = player.getAttribute('opt');
+  const enemyOpt = enemy.getAttribute('opt');
+  let message = '';
 
-   if (playerOpt === enemyOpt) {
-     message = 'Tie!';
-   } else if (
-     (playerOpt === 'rock' && enemyOpt === 'scissor') ||
-     (playerOpt === 'paper' && enemyOpt === 'rock') ||
-     (playerOpt === 'scissor' && enemyOpt === 'paper')
-   ) {
-     message = 'You won!';
-   } else {
-     message = 'You lost!';
-   }
+  if (playerOpt === enemyOpt) {
+    message = 'Tie!';
+  } else if (
+    (playerOpt === 'rock' && enemyOpt === 'scissor') ||
+    (playerOpt === 'paper' && enemyOpt === 'rock') ||
+    (playerOpt === 'scissor' && enemyOpt === 'paper')
+  ) {
+    message = 'You won!';
+    playerScore++;
+  } else {
+    message = 'You lost!';
+    enemyScore++;
+  }
 
-   result.textContent = message;
+  result.textContent = message;
+
+  // Update score
+  const playerScoreElem = document.querySelector('.player-score');
+  const enemyScoreElem = document.querySelector('.enemy-score');
+  playerScoreElem.textContent = playerScore;
+  enemyScoreElem.textContent = enemyScore;
+
+  // Show play again button
+  playAgainBtn.style.display = 'block';
 };
 
 options.forEach((option) => {
-   option.addEventListener('click', () => {
-     playerOptions.innerHTML = '';
-     playerOptions.appendChild(option.cloneNode(true));
-     getRandomOption();
+  option.addEventListener('click', () => {
+    playerOptions.innerHTML = '';
+    playerOptions.appendChild(option.cloneNode(true));
+    getRandomOption();
 
-     setTimeout(() => {
-       const player = document.querySelector('.player-options .option img');
-       const enemy = document.querySelector('.enemy-options .option img');
-       showResult(player, enemy);
-     }, 1500);
-   });
+    setTimeout(() => {
+      const player = document.querySelector('.player-options .option img');
+      const enemy = document.querySelector('.enemy-options .option img');
+      showResult(player, enemy);
+    }, 1500);
+  });
 });
+
